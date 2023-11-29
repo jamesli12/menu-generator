5dd87d5be854f35bb48282a058144cace0a7a70d
AZXEthan <anzexiu@gmail.com>
2023-11-28 21:16:20 -0500

    frontend

--- menu-gen-frontend/src/pages/RegisterForm.tsx
+++ menu-gen-frontend/src/pages/RegisterForm.tsx
@@ -1,95 +1,95 @@
 import { useState, FormEvent } from "react";
 import { Input, Button } from "@nextui-org/react";
 
-interface AccountFormProps {
-  onLogin: (data: {}) => void;
+interface SignupFormProps {
+    onAuthenticate: (credentials: {}) => void;
 }
 
-interface Account {
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
+interface UserCredentials {
+    userEmail: string;
+    userPassword: string;
+    userFirstName: string;
+    userLastName: string;
 }
 
-const AccountForm: React.FC<AccountFormProps> = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+const SignupForm: React.FC<SignupFormProps> = ({ onAuthenticate }) => {
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [userFirstName, setUserFirstName] = useState("");
+    const [userLastName, setUserLastName] = useState("");
 
-  const register = async (account: Account) => {
-    const formData = new FormData();
-    formData.append('email', account.email);
-    formData.append('password', account.password);
-    formData.append('firstName', account.firstName);
-    formData.append('lastName', account.lastName);
+    const handleRegistration = async (credentials: UserCredentials) => {
+        const registrationData = new FormData();
+        registrationData.append('email', credentials.userEmail);
+        registrationData.append('password', credentials.userPassword);
+        registrationData.append('firstName', credentials.userFirstName);
+        registrationData.append('lastName', credentials.userLastName);
 
-    const response = await fetch(import.meta.env["VITE_BACKEND_URI"] + "/register", {
-      method: "POST",
-      body: formData,
-    });
+        const serverResponse = await fetch(import.meta.env["VITE_BACKEND_URI"] + "/register", {
+            method: "POST",
+            body: registrationData,
+        });
 
-    if (response.ok) {
-      console.log("Registration successful");
-      onLogin(await response.json());
-    } else {
-      console.error("Registration failed");
-    }
-  };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    const account = {
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      password: password,
+        if (serverResponse.ok) {
+            console.log("Registration successful");
+            onAuthenticate(await serverResponse.json());
+        } else {
+            console.error("Registration failed");
+        }
     };
 
-    register(account);
-  };
+    const handleFormSubmit = (event: FormEvent) => {
+        event.preventDefault();
 
-  return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-      <Input
-        isRequired
-        type="email"
-        label="Email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        className="max-w-xs"
-      />
-      <Input
-        isRequired
-        label="Password"
-        variant="bordered"
-        placeholder="Enter your password"
-        type="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        className="max-w-xs"
-      />
-      <Input
-        isRequired
-        label="First Name"
-        value={firstName}
-        onChange={(event) => setFirstName(event.target.value)}
-        className="max-w-xs"
-      />
-      <Input
-        isRequired
-        label="Last Name"
-        value={lastName}
-        onChange={(event) => setLastName(event.target.value)}
-        className="max-w-xs"
-      />
-      <Button color="primary" type="submit" fullWidth>
-        Register
-      </Button>
-    </form>
-  );
+        const credentials = {
+            userEmail: userEmail,
+            userFirstName: userFirstName,
+            userLastName: userLastName,
+            userPassword: userPassword,
+        };
+
+        handleRegistration(credentials);
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
+            <Input
+                isRequired
+                type="email"
+                label="Email"
+                value={userEmail}
+                onChange={(event) => setUserEmail(event.target.value)}
+                className="max-w-xs"
+            />
+            <Input
+                isRequired
+                label="Password"
+                variant="bordered"
+                placeholder="Enter your password"
+                type="password"
+                value={userPassword}
+                onChange={(event) => setUserPassword(event.target.value)}
+                className="max-w-xs"
+            />
+            <Input
+                isRequired
+                label="First Name"
+                value={userFirstName}
+                onChange={(event) => setUserFirstName(event.target.value)}
+                className="max-w-xs"
+            />
+            <Input
+                isRequired
+                label="Last Name"
+                value={userLastName}
+                onChange={(event) => setUserLastName(event.target.value)}
+                className="max-w-xs"
+            />
+            <Button color="primary" type="submit" fullWidth>
+                Sign Up
+            </Button>
+        </form>
+    );
 };
 
-export default AccountForm;
+export default SignupForm;
